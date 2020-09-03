6ad854379b641a57ce91a05c03056434f74e248b
Ramtin <ramtin.abadi@gmail.com>
2020-09-03 12:05:10 +0530

    Added basic CLI structure

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
 #!/usr/bin/env node
+
+// Node
+import os from "os";
+import fs from 'fs';
+
+// Third Party
+import {Command} from "commander";
+
+
+let globalRegistry = os.homedir() + '/.endemism_registry';
+let program = new Command();
+
+
+// General CLI
+program.version('0.0.1');
+
+
+// List
+program
+	.command('list')
+	.description("Lists the name and versions of the package in registry")
+	.option('-g, --global', "Lists the packages in the global registry", true)
+	.option('-p, --project', "Lists the packages in the project registry", false)	
+	.action((options: {global: boolean, project: boolean}) => {
+		
+	});
+
+
+program.parse(process.argv);
